2c3274f2541263311afbf1348cc2f78b33071425
frylstar <chengxiaszz@sina.com>
2022-03-24 00:14:28 +0800

    feat: add husky、commitlint、lint-stage

--- .prettierrc.js
+++ .prettierrc.js
@@ -6,6 +6,7 @@ module.exports = {
   bracketSpacing: true, // Print spaces between brackets in object literals.
   // Put the > of a multi-line JSX element at the end of the last line instead of being alone on the next line (does not apply to self closing elements).
   // jsxBracketSameLine: false,
-  arrowParens: 'avoid', // Include parentheses around a sole arrow function parameter
-  htmlWhitespaceSensitivity: 'ignore',
+  arrowParens: "avoid", // Include parentheses around a sole arrow function parameter
+  htmlWhitespaceSensitivity: "ignore",
+  // endOfLine: "auto",
 }
